459fed12f6ef69e520311e9695bf0eef2389a479
Alex Trimper <trimper.a@gmail.com>
2022-11-08 15:03:10 -0400

    Created links to each task on index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def find
+    @tasks = Task.find(params[:id])
+    redirect_to find_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#find', as: :find
 end
