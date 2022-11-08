edcc48bdd38cd13a9f03625388dab8263a9bb159
Alex Trimper <trimper.a@gmail.com>
2022-11-08 17:50:23 -0400

    completed edit feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,32 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit]
   def index
     @tasks = Task.all
   end
 
-  def find
-    @tasks = Task.find(params[:id])
-    redirect_to find_path
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#find', as: :find
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
